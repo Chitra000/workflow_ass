6eff905bf12b89f1ab285b1cad2cc205dd0ba360
Chitra000 <99902398+Chitra000@users.noreply.github.com>
2023-10-15 19:38:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "test-bucket2233"
+    bucket         = "chitra-bucket-12"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "karan01"
